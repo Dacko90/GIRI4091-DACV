0681fea003fd7e301f9fa4502d9a2de7b8c5d658
Dacko90 <lrmdacko@gmail.com>
2024-07-01 18:50:07 -0400

    Chicharronera

---
+++ Ejercicios/chicharronera.py
@@ -0,0 +1,11 @@
+a = float(input('Ingrsa el valor de "A" \n'))
+b = float(input('Ingrsa el valor de "B" \n'))
+c = float(input('Ingrsa el valor de "C" \n'))
+
+x1 = (-b + (b*b - 4*a*c)**0.5)/(2*a)
+x2 = (-b - (b*b - 4*a*c)**0.5)/(2*a)
+
+
+
+
+print (x1, ' ', x2)
